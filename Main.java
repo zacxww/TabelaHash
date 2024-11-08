27341dcb8341b5562cfacd2c1a28d32fd231ae91
Zaca <binnhozacarias@gmail.com>
2024-11-08 10:03:37 -0300

    relatorio no console

--- Main.java
+++ Main.java
@@ -4,11 +4,41 @@ public class Main {
 
     public static void main(String[] args) {
 
-        HashTable<String, String> tabela = new HashTable1<>(100);
+        HashTable<String, String> tabela1 = new HashTable1<>(5000);
+        HashTable<String, String> tabela2 = new HashTable2<>(5000);
 
         String caminho = "female_names.txt";
-        tabela.lerTXT(caminho);
 
-        tabela.display();
+        long inicioInsersao1 = System.nanoTime();
+        tabela1.lerTXT(caminho);
+        long fimInsersao1 = System.nanoTime();
+        long totalInsercao1 = fimInsersao1 - inicioInsersao1;
+
+        //tabela1.display(); // PRINT DA TABELA SE NECESSARIO
+
+        System.out.println("TABELA 1:");
+        System.out.println("----------------------------//----------------------------");
+        System.out.println("Tempo de Insercao: " + totalInsercao1 + "ns");
+        System.out.println("----------------------------//----------------------------");
+        System.out.println("Colisoes: " + tabela1.getColisoes());
+        System.out.println("----------------------------//----------------------------");
+        tabela1.numChaves();
+
+        long inicioInsersao2 = System.nanoTime();
+        tabela2.lerTXT(caminho);
+        long fimInsersao2 = System.nanoTime();
+        long totalInsercao2 = fimInsersao2 - inicioInsersao2;
+
+        //tabela2.display(); // PRINT DA TABELA SE NECESSARIO
+
+        System.out.println("TABELA 2:");
+        System.out.println("----------------------------//----------------------------");
+        System.out.println("Tempo de Insercao: " + totalInsercao2 + "ns");
+        System.out.println("----------------------------//----------------------------");
+        System.out.println("Colisoes: " + tabela2.getColisoes());
+        System.out.println("----------------------------//----------------------------");
+        tabela2.numChaves();
+
+        // COM MUITO AMOR LUCAS ZACARIAS
     }
 }
--- hashtable/HashTable.java
+++ hashtable/HashTable.java
@@ -9,11 +9,13 @@ public abstract class HashTable<K, V> {
     protected HashNode<K, V>[] tabela;
     protected int capacidade;
     protected int tamanho;
+    private int colisoes;
 
     public HashTable(int capacity) {
         this.capacidade = capacity;
         this.tabela = new HashNode[capacity];
         this.tamanho = 0;
+        this.colisoes = 0;
     }
 
     protected abstract int hash(K key);
@@ -21,6 +23,9 @@ public abstract class HashTable<K, V> {
     public void put(K key, V value) {
         int index = hash(key);
         HashNode<K, V> head = tabela[index];
+        if (head != null){
+            colisoes++;
+        }
         while (head != null) {
             if (head.key.equals(key)) {
                 head.value = value;
@@ -81,6 +86,22 @@ public abstract class HashTable<K, V> {
         return head.value;
     }
 
+    public void numChaves() {
+        System.out.println("Quantidade de elementos em cada chave:");
+        for (int i = 0; i < capacidade; i++) {
+            int counter = 0;
+            HashNode<K, V> head = tabela[i];
+            while (head != null) {
+                counter++;
+                head = head.next;
+            }
+            System.out.print( i + ": " + counter + " elementos" + " | ");
+            if (i == capacidade/2) {
+                System.out.print("\n");
+            }
+        }
+    }
+
     public int size() {
         return tamanho;
     }
@@ -99,5 +120,9 @@ public abstract class HashTable<K, V> {
             }
             System.out.println();
         }
+    }
+
+    public int getColisoes() {
+        return colisoes;
     }
 }
